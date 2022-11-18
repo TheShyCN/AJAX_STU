00cf21082f30e76e449d760f192776f49b316247
Riven <2452466220@qq.com>
2022-11-18 20:14:17 +0800

    NO.3 commit

--- 原生AJAX/server.js
+++ 原生AJAX/server.js
@@ -43,6 +43,17 @@ app.post("/test_post/:name/:age", (req, res) => {
   res.send("hello, post!");
 });
 
+app.get("/test_jquery_get_person", (req, res) => {
+  console.log(req.query);
+  const person = { name: "Riven", age: 21, sex: "女" };
+  res.send(JSON.stringify(person));
+});
+
+app.post("/test_jquery_post", (req, res) => {
+  console.log("请求体参数:", req.body);
+  res.send("jquery的post请求");
+});
+
 app.listen(8080, (err) => {
   if (!err) {
     console.log("测试ajax的服务器启动成功!");
